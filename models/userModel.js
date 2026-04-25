b9a026207c171e7c4b6eba7d15001992414f4538
Nimesha Jayawickrama <nimeshakavindhi4@gmail.com>
2026-04-26 00:06:01 +0530

    user role

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["Customer", "RestaurantOwner", "deliveryPersonnel"], default: "customer" },
+  role: { type: String, enum: ["Customer", "user" , "RestaurantOwner", "deliveryPersonnel"], default: "customer" },
 });
 
 const User = mongoose.model("User", userSchema);
