5da835e012ed46acc83dccaa94750767438c97b5
Nimesha Jayawickrama <nimeshakavindhi4@gmail.com>
2026-04-26 00:22:35 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,12 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["Customer", "user" , "RestaurantOwner", "deliveryPersonnel"], default: "customer" },
+  role: { 
+    type: String, 
+    enum: ["customer", "user", "restaurantowner", "deliverypersonnel"], 
+    default: "customer", 
+    set: v => v && typeof v === 'string' ? v.toLowerCase() : v
+  },
 });
 
 const User = mongoose.model("User", userSchema);
